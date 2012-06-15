51ec4c5aaec72b463b0d19a5b116fc097f6a8dfb
Dennis Suratna <dsuratna@event-10-1-49-95.venue.apple.com>
2012-06-15 10:46:26 -0700

    Initial Commit

---
+++ MusicPlayer/MusicPlayerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MusicPlayerAppDelegate.h
+//  MusicPlayer
+//
+//  Created by Dennis Suratna on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicPlayerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusicPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicPlayer
+//
+//  Created by Dennis Suratna on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MusicPlayerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MusicPlayerAppDelegate class]));
+    }
+}
