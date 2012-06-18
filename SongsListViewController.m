16283e3c71758e01b2903c405e10764355aadadf
Dennis Suratna <dennis.suratna@gmail.com>
2012-06-17 20:32:00 -0700

    Changing background image of UITableViewCell

--- SongsListViewController.m
+++ SongsListViewController.m
@@ -9,7 +9,6 @@
 #import "SongsListViewController.h"
 #import "SongListTableViewCell.h"
 #import <MediaPlayer/MediaPlayer.h>
-#import <CoreGraphics/CoreGraphics.h>
 
 @interface SongsListViewController ()
 
@@ -24,12 +23,15 @@
         isEditing = false;
         [[self tableView] setContentInset:UIEdgeInsetsMake(44.0, 0, 0, 0)];
         UINavigationBar * uiNavigationBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0.0f, -44, 320.0f, 44.0f)];
+        [uiNavigationBar setTintColor:[UIColor blackColor]];
         UINavigationItem * item = [[UINavigationItem alloc] initWithTitle:@"Playlists"];
-        editButton = [[UIBarButtonItem alloc] initWithTitle:@"Edit" style:UIBarButtonItemStyleDone target:nil action:@selector(editPlaylists:)];
-        [item setLeftBarButtonItem:editButton];
+//        editButton = [[UIBarButtonItem alloc] initWithTitle:@"Edit" style:UIBarButtonItemStyleDone target:nil action:@selector(editPlaylists:)];
+//        [item setLeftBarButtonItem:editButton];
         [uiNavigationBar pushNavigationItem:item animated:NO];
         [[self tableView] addSubview:uiNavigationBar];
         [[self tableView] setRowHeight:75.0];
+
+        [[self tableView] setBackgroundView:nil];
 
         MPMediaQuery * allSongsQuery = [[MPMediaQuery alloc] init];
         songs = [allSongsQuery items];
@@ -59,6 +61,7 @@
 }
 
 - (void)viewWillAppear:(BOOL)animated {
+    [self.tableView setSeparatorColor:[UIColor darkGrayColor]];
     [super viewWillAppear:animated];
 }
 
@@ -74,8 +77,8 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     NSLog(@"Called with section: %d", section);
-//    return [songs count];
-    return 3;
+    return [songs count];
+//    return 3;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -89,24 +92,27 @@
         songCell = nil;
     }
 
-//    MPMediaItem * song = [songs objectAtIndex:[indexPath row]];
+
+    reusableCell.backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"cellBackground.png"]];
+    reusableCell.selectedBackgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"cellBackgroundActive.png"]];
 
     UILabel * songTitleLabel = (UILabel *)[reusableCell viewWithTag:1];
     UILabel * artistLabel = (UILabel *)[reusableCell viewWithTag:2];
 
-    songTitleLabel.text = @"The Saga";
-    artistLabel.text = @"Gareth Emery";
+    MPMediaItem * song = [songs objectAtIndex:[indexPath row]];
+    songTitleLabel.text = [song valueForProperty:MPMediaItemPropertyTitle];
+    artistLabel.text = [song valueForProperty:MPMediaItemPropertyArtist];
 
     return reusableCell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"Song at position %d is selected. Title: %@", indexPath.row, [[songs objectAtIndex:indexPath.row] valueForProperty:MPMediaItemPropertyTitle]);
+}
+
+
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.row == 0){
-        [cell setBackgroundColor:[UIColor brownColor]];
-        [cell setFrame:CGRectMake(cell.frame.origin.x, cell.frame.origin.y, cell.frame.size.width, cell.frame.size.height + 100)];
-    }else{
-        [cell setBackgroundColor:[UIColor blackColor]];
-    }
+    [cell setBackgroundColor:[UIColor blackColor]];
 }
 
 
